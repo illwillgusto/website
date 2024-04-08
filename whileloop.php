cf7977bee6209a7f443ac8f05114ffe59a7561d1
Will Walton <will@endless3dge.com>
2024-04-08 15:05:43 -0500

    Create whileloop.php
    brief understanding of while loops, which are similar to how they are used in JS

---
+++ whileloop.php
@@ -0,0 +1,19 @@
+<?php
+/*While loop
+do some code infinitely while some condition remains true 
+ */
+
+$seconds = 0;
+$running = true;
+
+// while($running){
+//     //wait 1 second
+//     $seconds++;
+//     echo $seconds . "<br>";
+//     // this code would run forever and cause problems 
+// }
+ 
+
+
+
+?>
