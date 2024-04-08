5d6c517442b4460dc6b57d1ab179dc82e4aaa4be
Will Walton <will@endless3dge.com>
2024-04-08 11:10:47 -0500

    Update variables.php
    introduced the float variable and 2 examples of the floating variable
    
    also introduced the use of the backslash \

--- variables.php
+++ variables.php
@@ -8,6 +8,10 @@ $age = 30; // this is an integer
 $users = 5;
 $quantity = 10;
 
+// float variables, float - a floating point number, a number that contains a decimal portion 
+$gpa = 3.2;
+$price = 4.95;
+
 echo " This name was set as a variable in our code editor {$name} <br>";
 echo "{$name} loves {$food} <br>";
 echo "Your email is {$email} <br>";
@@ -15,4 +19,7 @@ echo "You are {$age} years old <br>";
 
 echo "There are {$users} users online right now <br>";
 echo "You would like to buy {$quantity} items from our store <br>";
+
+echo "Your gpa is {$gpa} <br>";
+echo "Your slice of pizza is \${$price} <br>"; // notice here the \ was used before the $
 // the syntax for calling a variable is {$variablename}
