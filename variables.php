3cfb34d4eec54b2153171d06481788fab9c2b7cb
Will Walton <will@endless3dge.com>
2024-04-08 11:24:03 -0500

    Update variables.php
    introduced booleans

--- variables.php
+++ variables.php
@@ -4,13 +4,18 @@ $name = "Will"; // set the variable name to my name
 $food = "wings";
 $email = "fake123@gmail.com";
 
-$age = 30; // this is an integer 
+$age = 30; // this is an integer, whole numbers 
 $users = 5;
 $quantity = 10;
 
 // float variables, float - a floating point number, a number that contains a decimal portion 
 $gpa = 3.2;
 $price = 4.95;
+
+// booleans, like in JS, they either true or false 
+$employed = true;
+$online = false;
+$for_sale = true;
 
 echo " This name was set as a variable in our code editor {$name} <br>";
 echo "{$name} loves {$food} <br>";
@@ -22,4 +27,8 @@ echo "You would like to buy {$quantity} items from our store <br>";
 
 echo "Your gpa is {$gpa} <br>";
 echo "Your slice of pizza is \${$price} <br>"; // notice here the \ was used before the $
+
+
+echo "Online status: {$online} <br>"; // because it is false nothing will be displayed on the web page 
+
 // the syntax for calling a variable is {$variablename}
