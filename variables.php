5e6c2d0c8875c3934569cefef88398ae93111891
Will Walton <will@endless3dge.com>
2024-04-08 10:58:07 -0500

    Update variables.php
    added more variables
    gave examples of variables being used in full sentences and provided the syntax on doing so

--- variables.php
+++ variables.php
@@ -1,7 +1,10 @@
 <?php
 echo "Variable = a reusable container that holds data <br>";
-$name = "Will Walton";
+$name = "Will"; // set the variable name to my name 
+$food = "wings";
+$email = "fake123@gmail.com";
 
-
-echo " This name was set as a variable in our code editor {$name}";
-?>
+echo " This name was set as a variable in our code editor {$name} <br>";
+echo "{$name} loves {$food} <br>";
+echo "Your email is {$email}";
+// the syntax for calling a variable is {$variablename}
