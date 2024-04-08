5d967fa6e9cb769301535029baf98930fc1e2092
Will Walton <will@endless3dge.com>
2024-04-08 10:41:17 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "I love pizza <br>";
+echo "Pizza is awesome <br>";
+echo "Pizza and fries is a undefeated combination";
+//this is a comment 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order a Pizza</button>
+</body>
+</html>
