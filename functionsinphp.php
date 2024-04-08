8cdd1fe35095d4965406131ba39850539bb6c453
Will Walton <will@endless3dge.com>
2024-04-08 18:04:32 -0500

    Update functionsinphp.php
    functions with return examples given
    work the same as in JS

--- functionsinphp.php
+++ functionsinphp.php
@@ -14,3 +14,18 @@ function happy_birthday($first_name, $age)
 }
 
 happy_birthday("Goku", 40); // call a function 
+
+
+function is_even($number){
+    return $result % 2;
+}
+
+echo is_even(10);
+
+
+function hypotenuse($a, $b){
+    $c = sqrt($a ** 2 + $b ** 2);
+    return $c;
+}
+
+echo hypotenuse(4, 5);
