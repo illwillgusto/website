c982cef1ae5e9803b986375b31b3b1e84fbe58ff
Will Walton <will@endless3dge.com>
2024-04-10 15:38:45 -0500

    Update session.php
    removed the echo statements with the session variables, they were used for testing purposes

--- session.php
+++ session.php
@@ -32,19 +32,18 @@ session_start();
 <?php
 if (isset($_POST["login"])) {
 
-    
-    if(!empty($_POST["username"]) && 
-    !empty($_POST["password"])){
-        
+
+    if (
+        !empty($_POST["username"]) &&
+        !empty($_POST["password"])
+    ) {
+
         $_SESSION["username"] = $_POST["username"];
         $_SESSION["password"] = $_POST["password"];
 
-        echo $_SESSION["username"] . "<br>";
-        echo $_SESSION["password"] . "<br>";
 
-    }
-    else{
-        echo "Missing username/password";
+    } else {
+        echo "Missing username/password <br>";
     }
 }
 ?>
