478a1dd2b21b0a0c810d41222a4e762e2a67c2c3
Will Walton <will@endless3dge.com>
2024-04-10 15:30:02 -0500

    login input
    added html and text to the home.php file
    
    added login inputs and login button to the session.php file

--- session.php
+++ session.php
@@ -5,26 +5,34 @@ A user is assigned a session-id
 ex. login credentials 
 */
 
-    session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sessions</title>
 </head>
+
 <body>
-    This is the login page<br>
-    <a href="home.php">This goes to the home page</a><br>
+    <form action="session.php" method="post">
+        username: <br>
+        <input type="text" name="username" require><br>
+        password: <br>
+        <input type="password" name="password" require><br>
+        <input type="submit" name="Submit"><br>
+    </form><br>
 </body>
+
 </html>
 
 <?php
-     $_SESSION["username"] = "Will";
-     $_SESSION["password"] = "gusto1234";
- 
-     echo $_SESSION["username"] . "<br>";
-     echo $_SESSION["password"] . "<br>";
+$_SESSION["username"] = "Will";
+$_SESSION["password"] = "gusto1234";
+
+echo $_SESSION["username"] . "<br>";
+echo $_SESSION["password"] . "<br>";
 ?>
