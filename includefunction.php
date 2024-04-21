cd1093768979916b623bca9781746b8df72569ac
Will Walton <will@endless3dge.com>
2024-04-20 19:30:55 -0500

    Update includefunction.php
    update to notes

--- includefunction.php
+++ includefunction.php
@@ -2,9 +2,9 @@
 /*
 include()
 Copies the content of a file (php/html/text)
-and includes it in your php file
-Sections of our website become reusable
-Changes only need to be made in one place 
+and includes it in your php file.
+Sections of our website become reusable.
+Changes only need to be made in one place
 */
 
 include("header.html");
