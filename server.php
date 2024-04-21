f393f88d904a1cc4339e5f12db22f1510336ac99
Will Walton <will@endless3dge.com>
2024-04-20 20:38:25 -0500

    Update server.php
    created html form
    
    commented out sample syntax for what the server method provides

--- server.php
+++ server.php
@@ -1,10 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Server Learning</title>
+</head>
+<body>
+  <form action="server.php" method="post">
+    username:<br>
+    <input type="text" name="username">
+    <input type="submit">
+  </form>
+</body>
+</html>
+
 <?php
 //  $_SERVER = SGB that contains headers, paths, and script locations.
 //             The entires in this array are created by the web server.
 //             Shows nearly everything you need to know about the current web page environment.
-
+/*
 foreach ($_SERVER as $key => $value) {
   echo"{$key} = {$value} <br>";
 }
-
+*/
 ?>
