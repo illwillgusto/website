bbadf7c780206dca80a73bdb06e861f711752502
Will Walton <will@endless3dge.com>
2024-04-24 11:33:22 -0500

    Update database.php
    introducing the try block for exception handling when you know you have some code that might throw an error

--- database.php
+++ database.php
@@ -7,6 +7,15 @@
   $db_name = "businessdb";
   $connection = "";
 
+  try {
+    $connection = mysqli_connect($db_server,
+    $db_user,
+    $db_pass,
+    $db_name); // trying some code that might throw an error
+  } catch (\Throwable $th) {
+    //throw $th;
+  } // the 'try block' deals with exception handling
+
   $connection = mysqli_connect($db_server,
   $db_user,
   $db_pass,
