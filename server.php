2198461c721f49365723f25b5721952996492a5d
Will Walton <will@endless3dge.com>
2024-04-24 11:17:12 -0500

    Update server.php
    comment update

--- server.php
+++ server.php
@@ -16,7 +16,7 @@ contains the value of the current file path, php_self is vulnerable to cross-sit
 </html>
 
 <?php
-//  $_SERVER = SGB that contains headers, paths, and script locations.
+//  $_SERVER = SGB (Super Global Variable) that contains headers, paths, and script locations.
 //             The entires in this array are created by the web server.
 //             Shows nearly everything you need to know about the current web page environment.
 
