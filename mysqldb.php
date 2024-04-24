16a376d9edec323df48e92c228ddfefc3d2c0f71
Will Walton <will@endless3dge.com>
2024-04-24 12:36:38 -0500

    Update mysqldb.php
    try block added for handling if a user can't be registered due to username or password aren't added

--- mysqldb.php
+++ mysqldb.php
@@ -4,7 +4,13 @@ include("database.php");
 $sql = "INSERT INTO users (user, password)
         VALUES ('Goku', 'password1')";
 
-mysqli_query($connection, $sql);
+
+try {
+  mysqli_query($connection, $sql);
+  echo "User is now registered.";
+} catch (mysqli_sql_exception) {
+  echo "Could not register user";
+}
 
 mysqli_close($connection);
 ?>
