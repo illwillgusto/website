d2ebf2212d7a846fa678a40d3d81f39bc39a569f
Will Walton <will@endless3dge.com>
2024-04-24 14:28:56 -0500

    Update projectreg.php
    added html script
    added form tags with the action of a php server script which will give us the name of the file

--- projectreg.php
+++ projectreg.php
@@ -4,6 +4,19 @@ include("database.php");
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="<?php $_SERVER["PHP_SELF"] ?>">
+
+  </form>
+</body>
+</html>
 <?php
   mysqli_close($connection);
 ?>
