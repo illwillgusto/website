c49693a0056e4e20c172b23511f4e6f4fb04949f
Will Walton <will@endless3dge.com>
2024-04-24 14:29:46 -0500

    Update projectreg.php
    added filter to avoid cross-site

--- projectreg.php
+++ projectreg.php
@@ -12,7 +12,7 @@ include("database.php");
   <title>Document</title>
 </head>
 <body>
-  <form action="<?php $_SERVER["PHP_SELF"] ?>">
+  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
 
   </form>
 </body>
