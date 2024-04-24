c05b3565b1b7ac549d227157e90e29010cb36766
Will Walton <will@endless3dge.com>
2024-04-24 12:16:39 -0500

    Update mysqldb.php
    removed the extra $ in the $sql
    removed the html script

--- mysqldb.php
+++ mysqldb.php
@@ -4,18 +4,7 @@ include("database.php");
 $sql = "INSERT INTO users (user, password)
         VALUES ('Goku', 'password1')";
 
-mysqli_query($connection, $$sql);
+mysqli_query($connection, $sql);
 
 mysqli_close($connection);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  Hello<br>
-</body>
-</html>
