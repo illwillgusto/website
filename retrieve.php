be92fd4d9f79e67319de25eebcfaeb9be6dee157
Will Walton <will@endless3dge.com>
2024-04-24 13:37:00 -0500

    Update retrieve.php
    created the result variable

--- retrieve.php
+++ retrieve.php
@@ -3,7 +3,7 @@ include('database.php');
 
 
 $sql = "SELECT * FROM users WHERE users = 'Goku'"; //this is telling us what we are selecting, from where and what
-mysqli_query($connection, $sql); // this function will return an object 
+$result = mysqli_query($connection, $sql); // this function will return an object
 
 
 mysqli_close($connection);
