1607e0fba88efd043aa2252f002d9c729ab7ca95
Will Walton <will@endless3dge.com>
2024-04-24 13:44:35 -0500

    Update retrieve.php
    fi statement added for determining the number of rows and the information in those rows being displayed

--- retrieve.php
+++ retrieve.php
@@ -5,6 +5,13 @@ include('database.php');
 $sql = "SELECT * FROM users WHERE users = 'Goku'"; //this is telling us what we are selecting, from where and what
 $result = mysqli_query($connection, $sql); // this function will return an object
 
+// finding out how many rows are in a query we will use an if statement
+if(mysqli_num_rows($result) > 0){
+  $row = mysqli_fetch_assoc($result); //this function returns the next available row within our object
+  echo $row['id'] . "<br>";
+  echo $row['user'] . "<br>";
+  echo $row['reg_date'] . "<br>";
+}
 
 mysqli_close($connection);
 ?>
