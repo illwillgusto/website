49e1d225227cdece64b5eb2ccdccc5a78f03e6c2
Will Walton <will@endless3dge.com>
2024-04-24 14:17:51 -0500

    Update retrieve.php
    while loop statement added this will allow us to return more than one row

--- retrieve.php
+++ retrieve.php
@@ -7,12 +7,14 @@ $result = mysqli_query($connection, $sql); // this function will return an objec
 
 // finding out how many rows are in a query we will use an if statement
 if(mysqli_num_rows($result) > 0){
-  $row = mysqli_fetch_assoc($result); //this function returns the next available row within our object
-  echo $row['id'] . "<br>";
-  echo $row['user'] . "<br>";
-  echo $row['reg_date'] . "<br>";
+  while($row = mysqli_fetch_assoc($result)){
+     echo $row['id'] . "<br>";
+     echo $row['user'] . "<br>";
+     echo $row['reg_date'] . "<br>";
+  }; //this function returns the next available row within our object
+
 } else {
-  echo "No user found"; // this will appear if a user entered isn't in our db 
+  echo "No user found"; // this will appear if a user entered isn't in our db
 }
 
 mysqli_close($connection);
