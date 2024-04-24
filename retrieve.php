da82d460e47ac410cdfec1e7f9eb84362c0abd67
Will Walton <will@endless3dge.com>
2024-04-24 13:36:03 -0500

    Update retrieve.php
    query function with the arguments where the connection is and the query to look for

--- retrieve.php
+++ retrieve.php
@@ -3,6 +3,8 @@ include('database.php');
 
 
 $sql = "SELECT * FROM users WHERE users = 'Goku'"; //this is telling us what we are selecting, from where and what
+mysqli_query($connection, $sql);
+
 
 mysqli_close($connection);
 ?>
